5cf6e5d6c9359f93ded4ce8b3822c46b7b651cbe
lookuss <ykm990625@gmail.com>
2022-11-19 02:07:16 +0900

    find goooooood data type for reverse gio dto

--- src/maps/dto/ReverseGioDto.dto.ts
+++ src/maps/dto/ReverseGioDto.dto.ts
@@ -1,12 +1,12 @@
-import {IsString} from 'class-validator';
+import {IsLatitude, IsLongitude, IsString} from 'class-validator';
 
 export class ReverseGioDto {
   @IsString()
   readonly id: string;
 
-  
+  @IsLatitude()
   readonly latitude: string;
 
-  
+  @IsLongitude()
   readonly longitude: string;
 }
