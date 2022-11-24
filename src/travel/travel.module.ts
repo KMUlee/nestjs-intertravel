ba874153bed82ca62b3871360f14c34d63b4e8f6
lookuss <ykm990625@gmail.com>
2022-11-24 19:28:31 +0900

    tt

--- src/travel/travel.module.ts
+++ src/travel/travel.module.ts
@@ -6,6 +6,7 @@ import { TravelsEntity } from 'src/entities/travels.entity';
 import { UserEntity } from '../entities/user.entity';
 import { TravelController } from './travel.controller';
 import { TravelService } from './travel.service';
+import {  PicsEntity } from 'src/entities/pics.entity';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { TravelService } from './travel.service';
     TypeOrmModule.forFeature([TravelListEntity]),
     TypeOrmModule.forFeature([TravelsEntity]),
     TypeOrmModule.forFeature([DiaryEntity]),
+    TypeOrmModule.forFeature([PicsEntity]),
   ],
   controllers: [TravelController],
   providers: [TravelService],
