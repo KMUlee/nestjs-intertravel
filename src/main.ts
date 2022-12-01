e98bf2114d16db3f466bf4cc6c0dd6c12372840b
lookuss <ykm990625@gmail.com>
2022-12-01 22:11:02 +0900

    tt

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import express from 'express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use('../inter-image', express.static(join(__dirname,'../inter-image')));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
