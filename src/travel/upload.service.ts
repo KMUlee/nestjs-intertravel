e92d9d551a39913a3043744324c0c9a88c8981fa
lookuss <ykm990625@gmail.com>
2022-12-01 22:19:16 +0900

    tt

--- src/travel/upload.service.ts
+++ src/travel/upload.service.ts
@@ -2,7 +2,6 @@ import {  Injectable } from "@nestjs/common";
 
 @Injectable()
 export class UploadService {
-    static uploadService: any;
     public uploadFile(file: string): string {
         const generatedFiles:string = file;
         return generatedFiles;
