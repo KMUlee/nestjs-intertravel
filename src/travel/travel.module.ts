6b48927a9d6eba870b5136bd2f20c2eae4397c31
lookuss <ykm990625@gmail.com>
2022-12-08 00:58:13 +0900

    tt

--- src/travel/travel.module.ts
+++ src/travel/travel.module.ts
@@ -1,18 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DiaryEntity } from 'src/entities/diary.entity';
 import { TravelsEntity } from 'src/entities/travels.entity';
 import { UserEntity } from '../entities/user.entity';
 import { TravelController } from './travel.controller';
 import { TravelService } from './travel.service';
-import {  PicsEntity } from 'src/entities/pics.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserEntity]),
     TypeOrmModule.forFeature([TravelsEntity]),
-    TypeOrmModule.forFeature([DiaryEntity]),
-    TypeOrmModule.forFeature([PicsEntity]),
   ],
   controllers: [TravelController],
   providers: [TravelService],
